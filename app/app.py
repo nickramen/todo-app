fbefc59d7dbec78be31b25a2e96ca7f399f98df6
nickramen <katherine.nicoleram@gmail.com>
2023-02-16 00:37:22 -0600

    added checkbox and code to add tasks(still in progress)

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+#import requests
 
 app = Flask(__name__,template_folder='template')
 
-@app.route('/index',methods=['GET'])
+@app.route('/index', methods=['GET'])
 def get_initial_data():
     # make the database connection and cursor object
     myConnection = sqlite3.connect("../db/src/database/mydb.sqlite3")
@@ -24,6 +25,29 @@ def get_initial_data():
     # Pass the days and tasks, along with the dictionary to the HTML template
     return render_template('index.html', days=days, tasks=tasks, tasks_by_day=tasks_by_day)
     #return tasks_by_day
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    
+    # make the database connection and cursor object
+    myConnection = sqlite3.connect("../db/src/database/mydb.sqlite3")
+    cursor = myConnection.cursor()
+    
+    # Get the task description and day ID from the form
+    task_description = request.form['task_description']
+    day_id = request.form['day_id']
+
+    # Get the last task ID from the database
+    last_id = cursor.execute('SELECT MAX(task_id) FROM tbTasks').fetchone()[0]
+    if last_id is None:
+        last_id = 0
+
+    # Insert the new task into the database
+    cursor.execute('INSERT INTO tbTasks VALUES (?, ?, ?)', (last_id + 1, task_description, day_id))
+    myConnection.commit()
+
+    # Redirect to the home page
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
