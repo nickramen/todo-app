6bc7dce3a841fecf24d21a85b0349a359adc2a7e
nickramen <katherine.nicoleram@gmail.com>
2023-02-16 21:32:46 -0600

    Delete task function added

--- app/app.py
+++ app/app.py
@@ -10,20 +10,20 @@ def index():
     # Create a dictionary that maps each day ID to a list of tasks
     # Pass the days and tasks, along with the dictionary to the HTML template
     
-    myConnection = sqlite3.connect("../db/src/database/mydb.sqlite3")
-    cursor = myConnection.cursor()
+    with sqlite3.connect("../db/src/database/mydb.sqlite3") as myConnection:
+        cursor = myConnection.cursor()
     
-    cursor.execute('SELECT day_id, day_description FROM tbDays')
-    days = cursor.fetchall()
-    
-    cursor.execute('SELECT task_id, task_description, task_status, day_id FROM tbTasks')
-    tasks = cursor.fetchall()
-    
-    tasks_by_day = {day[0]: [] for day in days}
-    for task in tasks:
-        tasks_by_day[task[3]].append((task[0], task[1], task[2]))
-    
-    return render_template('index.html', days=days, tasks=tasks, tasks_by_day=tasks_by_day)
+        cursor.execute('SELECT day_id, day_description FROM tbDays')
+        days = cursor.fetchall()
+        
+        cursor.execute('SELECT task_id, task_description, task_status, day_id FROM tbTasks')
+        tasks = cursor.fetchall()
+        
+        tasks_by_day = {day[0]: [] for day in days}
+        for task in tasks:
+            tasks_by_day[task[3]].append((task[0], task[1], task[2]))
+        
+        return render_template('index.html', days=days, tasks=tasks, tasks_by_day=tasks_by_day)
 
 
 @app.route('/add_task', methods=['POST'])
@@ -34,20 +34,20 @@ def add_task():
     # Insert the new task into the database with status set as undone (0)
     # Redirect to the home page
     
-    myConnection = sqlite3.connect("../db/src/database/mydb.sqlite3")
-    cursor = myConnection.cursor()
+    with sqlite3.connect("../db/src/database/mydb.sqlite3") as myConnection:
+        cursor = myConnection.cursor()
     
-    task_description = request.form['task_description']
-    day_id = request.form['day_id']
+        task_description = request.form['task_description']
+        day_id = request.form['day_id']
+        
+        last_id = cursor.execute('SELECT MAX(task_id) FROM tbTasks').fetchone()[0]
+        if last_id is None:
+            last_id = 0
+        
+        cursor.execute('INSERT INTO tbTasks VALUES (?, ?, ?, ?)', (last_id + 1, task_description, 0, day_id))
+        myConnection.commit()
     
-    last_id = cursor.execute('SELECT MAX(task_id) FROM tbTasks').fetchone()[0]
-    if last_id is None:
-        last_id = 0
-    
-    cursor.execute('INSERT INTO tbTasks VALUES (?, ?, ?, ?)', (last_id + 1, task_description, 0, day_id))
-    myConnection.commit()
-    
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
 @app.route('/task_status', methods=['POST'])
 def task_status():
@@ -58,24 +58,39 @@ def task_status():
     # Update task status using the id
     # Redirect to the home page
     
-    myConnection = sqlite3.connect("../db/src/database/mydb.sqlite3")
-    cursor = myConnection.cursor()
+    with sqlite3.connect("../db/src/database/mydb.sqlite3") as myConnection:
+        cursor = myConnection.cursor()
     
-    taskId = request.get_data(as_text=True)
-    print(taskId)
-    taskStatus = cursor.execute('SELECT task_status FROM tbTasks WHERE task_id == ?', (taskId,)).fetchone()[0]
+        taskId = request.get_data(as_text=True)
+        taskStatus = cursor.execute('SELECT task_status FROM tbTasks WHERE task_id == ?', (taskId,)).fetchone()[0]
+        
+        if(taskStatus == 1):
+            cursor.execute('UPDATE tbTasks SET task_status = ? WHERE task_id = ?', (0, taskId))
+            myConnection.commit()
+        elif(taskStatus == 0):
+            cursor.execute('UPDATE tbTasks SET task_status = ? WHERE task_id = ?', (1, taskId))
+            myConnection.commit()
+        else:
+            return "Failed on updating task status"
     
-    if(taskStatus == 1):
-        cursor.execute('UPDATE tbTasks SET task_status = ? WHERE task_id = ?', (0, taskId))
-        myConnection.commit()
-    elif(taskStatus == 0):
-        cursor.execute('UPDATE tbTasks SET task_status = ? WHERE task_id = ?', (1, taskId))
-        myConnection.commit()
-    else:
-        return "Failed on updating task status"
-    
-    return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
+
+@app.route('/delete_task', methods=['POST'])
+def delete_task():
+    
+    # Make the database connection and cursor object
+    # Get the id from the request body
+    # Delete task status using the id
+    # Redirect to the home page
+    
+    with sqlite3.connect("../db/src/database/mydb.sqlite3") as myConnection:
+        cursor = myConnection.cursor()
+        
+        taskId = request.get_data(as_text=True)
+        cursor.execute('DELETE FROM tbTasks WHERE task_id == ?', (taskId,))
+        
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
