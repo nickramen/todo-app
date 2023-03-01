5121219c98ae6480ddfc0a6fd449ef71dd5e8f20
nickramen <katherine.nicoleram@gmail.com>
2023-03-01 01:40:21 -0600

    modified tbtasks, added 3 fields and respective values in sample inserts

--- db/src/initial_data.py
+++ db/src/initial_data.py
@@ -42,7 +42,10 @@ try:
             task_status INTEGER(1),
             day_id INTEGER REFERENCES tbDays(day_id),
             cat_id INTEGER REFERENCES tbCategories(cat_id),
-            user_id INTEGER REFERENCES tbUsers(user_id)
+            user_id INTEGER REFERENCES tbUsers(user_id),
+            create_date DATETIME,
+            edit_date DATETIME NULL,
+            is_deleted INTEGER(1) DEFAULT 0
         );
         
         INSERT INTO tbRoles VALUES (1,'Administrator');
@@ -78,69 +81,69 @@ try:
         INSERT INTO tbCategories VALUES (6,'Home');
         INSERT INTO tbCategories VALUES (7,'Travel');
         
-        INSERT INTO tbTasks VALUES (1, 'Complete project report', 1, 1, 1, 2);
-        INSERT INTO tbTasks VALUES (2, 'Attend team meeting', 1, 2, 2, 2);
-        INSERT INTO tbTasks VALUES (3, 'Submit expense report', 0, 3, 3, 2);
-        INSERT INTO tbTasks VALUES (4, 'Review code changes', 1, 4, 4, 2);
-        INSERT INTO tbTasks VALUES (5, 'Prepare for client presentation', 1, 5, 5, 2);
-        INSERT INTO tbTasks VALUES (6, 'Update website content', 0, 6, 6, 2);
-        INSERT INTO tbTasks VALUES (7, 'Call vendor for new equipment', 1, 7, 7, 2);
-        INSERT INTO tbTasks VALUES (8, 'Draft marketing email', 1, 1, 1, 2);
-        INSERT INTO tbTasks VALUES (9, 'Attend project kickoff meeting', 1, 2, 2, 2);
-        INSERT INTO tbTasks VALUES (10, 'Review performance metrics', 0, 3, 3, 2);
-        INSERT INTO tbTasks VALUES (11, 'Meet with vendor for product demo', 0, 7, 1, 2);
-        INSERT INTO tbTasks VALUES (12, 'Complete software installation', 1, 2, 2, 2);
-        INSERT INTO tbTasks VALUES (13, 'Prepare for final exams', 0, 3, 3, 2);
-        INSERT INTO tbTasks VALUES (14, 'Create workout plan for the week', 1, 4, 4, 2);
-        INSERT INTO tbTasks VALUES (15, 'Organize social event for team', 1, 5, 5, 2);
-        INSERT INTO tbTasks VALUES (16, 'Clean the house', 0, 6, 6, 2);
-        INSERT INTO tbTasks VALUES (17, 'Book flight tickets for vacation', 1, 7, 7, 2);
-        INSERT INTO tbTasks VALUES (18, 'Write blog post for website', 1, 1, 1, 2);
-        INSERT INTO tbTasks VALUES (19, 'Prepare for client demo', 1, 2, 2, 2);
-        INSERT INTO tbTasks VALUES (20, 'Submit research paper', 0, 3, 3, 2);
-        INSERT INTO tbTasks VALUES (21, 'Create exercise routine for the week', 1, 4, 4, 2);
-        INSERT INTO tbTasks VALUES (22, 'Plan team building activity', 1, 5, 5, 2);
-        INSERT INTO tbTasks VALUES (23, 'Organize closet', 0, 6, 6, 2);
-        INSERT INTO tbTasks VALUES (24, 'Book hotel for vacation', 1, 7, 7, 2);
-        INSERT INTO tbTasks VALUES (25, 'Write code for new feature', 1, 1, 1, 2);
-        INSERT INTO tbTasks VALUES (26, 'Attend training session', 1, 2, 2, 2);
-        INSERT INTO tbTasks VALUES (27, 'Research new marketing strategy', 0, 3, 3, 2);
-        INSERT INTO tbTasks VALUES (28, 'Schedule appointments with clients', 1, 4, 4, 2);
-        INSERT INTO tbTasks VALUES (29, 'Plan company event', 1, 5, 5, 2);
-        INSERT INTO tbTasks VALUES (30, 'Clean out the garage', 0, 6, 6, 2);
-        INSERT INTO tbTasks VALUES (31, 'Book rental car for vacation', 1, 7, 7, 2);
-        INSERT INTO tbTasks VALUES (32, 'Review website analytics', 1, 1, 1, 2);
-        INSERT INTO tbTasks VALUES (33, 'Meet with team for project status update', 1, 2, 2, 2);
-        INSERT INTO tbTasks VALUES (34, 'Create sales report', 0, 3, 3, 2);
-        INSERT INTO tbTasks VALUES (35, 'Run 5 miles', 1, 4, 4, 2);
-        INSERT INTO tbTasks VALUES (36, 'Plan team outing', 1, 5, 5, 2);
-        INSERT INTO tbTasks VALUES (37, 'Organize bookshelf', 0, 6, 6, 2);
-        INSERT INTO tbTasks VALUES (38, 'Book vacation rental', 1, 7, 7, 2);
+        INSERT INTO tbTasks VALUES (1, 'Complete project report', 1, 1, 1, 2, '2023-09-20 15:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (2, 'Attend team meeting', 1, 2, 2, 2, '2022-12-01 08:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (3, 'Submit expense report', 0, 3, 3, 2, '2022-05-10 12:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (4, 'Review code changes', 1, 4, 4, 2, '2023-01-05 18:20:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (5, 'Prepare for client presentation', 1, 5, 5, 2, '2022-06-15 10:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (6, 'Update website content', 0, 6, 6, 2, '2022-08-30 13:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (7, 'Call vendor for new equipment', 1, 7, 7, 2, '2023-05-23 09:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (8, 'Draft marketing email', 1, 1, 1, 2, '2023-03-18 16:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (9, 'Attend project kickoff meeting', 1, 2, 2, 2, '2022-07-02 11:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (10, 'Review performance metrics', 0, 3, 3, 2, '2023-02-07 14:10:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (11, 'Meet with vendor for product demo', 0, 7, 1, 2, '2022-09-22 17:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (12, 'Complete software installation', 1, 2, 2, 2, '2022-11-08 08:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (13, 'Prepare for final exams', 0, 3, 3, 2, '2023-08-03 13:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (14, 'Create workout plan for the week', 1, 4, 4, 2, '2023-01-18 11:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (15, 'Organize social event for team', 1, 5, 5, 2, '2022-05-25 14:20:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (16, 'Clean the house', 0, 6, 6, 2, '2022-10-07 09:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (17, 'Book flight tickets for vacation', 1, 7, 7, 2, '2023-06-08 12:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (18, 'Write blog post for website', 1, 1, 1, 2, '2023-03-02 16:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (19, 'Prepare for client demo', 1, 2, 2, 2, '2022-07-12 10:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (20, 'Submit research paper', 0, 3, 3, 2, '2022-12-20 14:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (21, 'Create exercise routine for the week', 1, 4, 4, 2, '2023-05-05 08:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (22, 'Plan team building activity', 1, 5, 5, 2, '2023-01-28 11:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (23, 'Organize closet', 0, 6, 6, 2, '2022-06-10 13:20:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (24, 'Book hotel for vacation', 1, 7, 7, 2, '2022-08-16 16:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (25, 'Write code for new feature', 1, 1, 1, 2, '2023-02-15 09:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (26, 'Attend training session', 1, 2, 2, 2, '2022-11-25 14:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (27, 'Research new marketing strategy', 0, 3, 3, 2, '2023-08-21 12:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (28, 'Schedule appointments with clients', 1, 4, 4, 2, '2023-01-09 10:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (29, 'Plan company event', 1, 5, 5, 2, '2022-05-31 11:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (30, 'Clean out the garage', 0, 6, 6, 2, '2022-09-12 15:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (31, 'Book rental car for vacation', 1, 7, 7, 2, '2023-03-30 13:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (32, 'Review website analytics', 1, 1, 1, 2, '2023-06-01 09:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (33, 'Meet with team for project status update', 1, 2, 2, 2, '2022-07-19 12:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (34, 'Create sales report', 0, 3, 3, 2, '2022-12-13 14:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (35, 'Run 5 miles', 1, 4, 4, 2, '2023-05-16 16:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (36, 'Plan team outing', 1, 5, 5, 2, '2023-02-22 08:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (37, 'Organize bookshelf', 0, 6, 6, 2, '2022-11-01 10:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (38, 'Book vacation rental', 1, 7, 7, 2, '2022-05-06 13:30:00', NULL, 0);
         
         
-        INSERT INTO tbTasks VALUES (39, 'Complete personal project', 1, 1, 1, 3);
-        INSERT INTO tbTasks VALUES (40, 'Read book on personal development', 1, 1, 1, 3);
-        INSERT INTO tbTasks VALUES (41, 'Meditate for 20 minutes', 0, 1, 1, 2);
-        INSERT INTO tbTasks VALUES (42, 'Meet with project team for status update', 1, 2, 2, 3);
-        INSERT INTO tbTasks VALUES (43, 'Prepare for client meeting', 1, 2, 2, 3);
-        INSERT INTO tbTasks VALUES (44, 'Attend training on new software', 1, 2, 2, 3);
-        INSERT INTO tbTasks VALUES (45, 'Follow up with customer support on issue', 1, 2, 2, 3);
-        INSERT INTO tbTasks VALUES (46, 'Research industry trends', 0, 2, 2, 3);
-        INSERT INTO tbTasks VALUES (47, 'Study for final exam', 0, 3, 3, 3);
-        INSERT INTO tbTasks VALUES (48, 'Write research paper on climate change', 1, 3, 3, 3);
-        INSERT INTO tbTasks VALUES (49, 'Attend office hours with professor', 0, 3, 3, 3);
-        INSERT INTO tbTasks VALUES (50, 'Review class notes for upcoming quiz', 0, 3, 3, 3);
-        INSERT INTO tbTasks VALUES (51, 'Go for a 5 mile run', 1, 4, 4, 3);
-        INSERT INTO tbTasks VALUES (52, 'Attend yoga class', 0, 4, 4, 3);
-        INSERT INTO tbTasks VALUES (53, 'Attend friends wedding', 1, 5, 5, 3);
-        INSERT INTO tbTasks VALUES (54, 'Clean kitchen', 1, 6, 6, 3);
-        INSERT INTO tbTasks VALUES (55, 'Do laundry', 1, 6, 6, 3);
-        INSERT INTO tbTasks VALUES (56, 'Organize closet', 1, 6, 6, 3);
-        INSERT INTO tbTasks VALUES (57, 'Wash dishes', 1, 6, 6, 3);
-        INSERT INTO tbTasks VALUES (58, 'Vacuum living room', 1, 6, 6, 3);
-        INSERT INTO tbTasks VALUES (59, 'Take out trash', 1, 6, 6, 3);
-        INSERT INTO tbTasks VALUES (60, 'Book flights for upcoming vacation', 1, 7, 7, 3);
-        INSERT INTO tbTasks VALUES (61, 'Research tourist attractions in new city', 0, 7, 7, 3);
+        INSERT INTO tbTasks VALUES (39, 'Complete personal project', 1, 1, 1, 3, '2022-08-23 11:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (40, 'Read book on personal development', 1, 1, 1, 3, '2023-01-20 09:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (41, 'Meditate for 20 minutes', 0, 1, 1, 2, '2023-07-13 14:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (42, 'Meet with project team for status update', 1, 2, 2, 3, '2023-02-28 16:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (43, 'Prepare for client meeting', 1, 2, 2, 3, '2022-06-28 09:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (44, 'Attend training on new software', 1, 2, 2, 3, '2022-09-08 12:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (45, 'Follow up with customer support on issue', 1, 2, 2, 3, '2023-03-13 10:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (46, 'Research industry trends', 0, 2, 2, 3, '2023-06-22 13:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (47, 'Study for final exam', 0, 3, 3, 3, '2022-07-26 15:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (48, 'Write research paper on climate change', 1, 3, 3, 3, '2023-09-20 15:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (49, 'Attend office hours with professor', 0, 3, 3, 3, '2022-12-01 08:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (50, 'Review class notes for upcoming quiz', 0, 3, 3, 3, '2022-05-10 12:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (51, 'Go for a 5 mile run', 1, 4, 4, 3, '2023-01-05 18:20:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (52, 'Attend yoga class', 0, 4, 4, 3, '2022-06-15 10:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (53, 'Attend friends wedding', 1, 5, 5, 3, '2022-08-30 13:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (54, 'Clean kitchen', 1, 6, 6, 3, '2023-05-23 09:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (55, 'Do laundry', 1, 6, 6, 3, '2023-03-18 16:15:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (56, 'Organize closet', 1, 6, 6, 3, '2022-07-02 11:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (57, 'Wash dishes', 1, 6, 6, 3, '2023-02-07 14:10:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (58, 'Vacuum living room', 1, 6, 6, 3, '2022-09-22 17:30:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (59, 'Take out trash', 1, 6, 6, 3, '2022-11-08 08:00:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (60, 'Book flights for upcoming vacation', 1, 7, 7, 3, '2023-08-03 13:45:00', NULL, 0);
+        INSERT INTO tbTasks VALUES (61, 'Research tourist attractions in new city', 0, 7, 7, 3, '2023-01-18 11:00:00', NULL, 0);
     """)
     
     # commit the changes and close the database connection 
